b3fe6818df086279455a27b7a447c3f9b58b145f
Vitor dos Santos Barcelos <progvitor@gmail.com>
2026-04-07 10:47:16 -0300

    sim

---
+++ Simulations/BLDC - VEL CONTROL Target files/ext-src/_c_function3/sources/c_for_typhoon.c
@@ -0,0 +1,101 @@
+#include "c_for_typhoon.h"
+#include <math.h>
+
+
+void resetSwitches (INVERSOR_SWITCHES* switches) {
+    switches->Sa_top = 0;
+    switches->Sa_bot = 0;
+    switches->Sb_top = 0;
+    switches->Sb_bot = 0;
+    switches->Sc_top = 0;
+    switches->Sc_bot = 0;
+}
+void updateHallInputs (
+    HALL_INPUTS* hallInputs,
+    unsigned char hallA,
+    unsigned char hallB,
+    unsigned char hallC
+) {
+    hallInputs->HallA = hallA;
+    hallInputs->HallB = hallB;
+    hallInputs->HallC = hallC;
+}
+
+void bldcHallSensor2phComLogic(
+    INVERSOR_SWITCHES* switches,
+    HALL_INPUTS hallInputs,
+    const Rotation_Direction direction
+) {
+
+    switch (direction) {
+
+        case CLOCKWISE: {
+            //0 - 60
+            if (hallInputs.HallA == 1 && hallInputs.HallB == 0 && hallInputs.HallC == 0) {
+                switches->Sc_top = 1;
+                switches->Sb_bot = 1;
+            }
+            // 60 - 120
+            if (hallInputs.HallA == 1 && hallInputs.HallB == 1 && hallInputs.HallC == 0) {
+                switches->Sa_top = 1;
+                switches->Sb_bot = 1;
+            }
+            // 120 - 180
+            if (hallInputs.HallA == 0 && hallInputs.HallB == 1 && hallInputs.HallC == 0) {
+                switches->Sa_top = 1;
+                switches->Sc_bot = 1;
+            }
+            // 180 - 240
+            if (hallInputs.HallA == 0 && hallInputs.HallB == 1 && hallInputs.HallC == 1) {
+                switches->Sb_top = 1;
+                switches->Sc_bot = 1;
+            }
+            // 240 - 300
+            if (hallInputs.HallA == 0 && hallInputs.HallB == 0 && hallInputs.HallC == 1) {
+                switches->Sb_top = 1;
+                switches->Sa_bot = 1;
+            }
+            // 300 - 360
+            if (hallInputs.HallA == 1 && hallInputs.HallB == 0 && hallInputs.HallC == 1) {
+                switches->Sc_top = 1;
+                switches->Sa_bot = 1;
+            }
+            break;
+        }
+
+
+        case COUNTER_CLOCKWISE: {
+
+            if (hallInputs.HallA == 1 && hallInputs.HallB == 0 && hallInputs.HallC == 0) {
+                switches->Sa_top = 1;
+                switches->Sc_bot = 1;
+            }
+            // 60 - 120
+            if (hallInputs.HallA == 1 && hallInputs.HallB == 1 && hallInputs.HallC == 0) {
+                switches->Sa_top = 1;
+                switches->Sb_bot = 1;
+            }
+            // 120 - 180
+            if (hallInputs.HallA == 0 && hallInputs.HallB == 1 && hallInputs.HallC == 0) {
+                switches->Sc_top = 1;
+                switches->Sb_bot = 1;
+            }
+            // 180 - 240
+            if (hallInputs.HallA == 0 && hallInputs.HallB == 1 && hallInputs.HallC == 1) {
+                switches->Sc_top = 1;
+                switches->Sa_bot = 1;
+            }
+            // 240 - 300
+            if (hallInputs.HallA == 0 && hallInputs.HallB == 0 && hallInputs.HallC == 1) {
+                switches->Sb_top = 1;
+                switches->Sa_bot = 1;
+            }
+            // 300 - 360
+            if (hallInputs.HallA == 1 && hallInputs.HallB == 0 && hallInputs.HallC == 1) {
+                switches->Sb_top = 1;
+                switches->Sc_bot = 1;
+            }
+            break;
+        }
+    }
+}
---
+++ Simulations/BLDC - VEL CONTROL with Current feedback Target files/ext-src/_c_function3/sources/c_for_typhoon.h
@@ -0,0 +1,39 @@
+#ifndef C_FOR_TYPHOON_LIBRARY_H
+#define C_FOR_TYPHOON_LIBRARY_H
+#include <stdint.h>
+
+typedef struct {
+    unsigned char Sa_top;
+    unsigned char Sa_bot;
+    unsigned char Sb_top;
+    unsigned char Sb_bot;
+    unsigned char Sc_top;
+    unsigned char Sc_bot;
+} INVERSOR_SWITCHES;
+
+typedef struct {
+    unsigned char HallA;
+    unsigned char HallB;
+    unsigned char HallC;
+} HALL_INPUTS;
+
+typedef enum  {
+    CLOCKWISE,
+    COUNTER_CLOCKWISE,
+} Rotation_Direction;
+
+void resetSwitches (INVERSOR_SWITCHES* switches);
+void updateHallInputs (
+    HALL_INPUTS* hallInputs,
+    unsigned char hallA,
+    unsigned char hallB,
+    unsigned char hallC
+);
+
+void bldcHallSensor2phComLogic (
+    INVERSOR_SWITCHES* switches,
+    HALL_INPUTS hallInputs,
+    Rotation_Direction direction
+);
+
+#endif // C_FOR_TYPHOON_LIBRARY_H
